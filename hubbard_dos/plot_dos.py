4ece576c3cf3ca8a089b61cdeefa5e11be74c785
Petar Bakalov <petar.baka@gmail.com>
2018-03-12 07:40:57 +0100

    default file list

--- hubbard_dos/plot_dos.py
+++ hubbard_dos/plot_dos.py
@@ -5,7 +5,14 @@ import h5py
 import sys
 plt.ion()
 
-file_list  = sys.argv[1:]
+if len(sys.argv)>1:
+    file_list  = sys.argv[1:]
+else:
+    file_list = ["dos_1d_ne100_nk1000000.h5",
+                 "dos_2d_ne101_nk8000.h5",
+                 "dos_3d_ne100_nk500.h5"]
+
+symbols = ['-x', '-o', '-s', '-d']
 
 fig = plt.figure("Free DOS")
 for i,filename in enumerate(file_list):
@@ -14,11 +21,11 @@ for i,filename in enumerate(file_list):
     e_vect = A["mesh"].value
     A.close()
     
-    plt.plot(e_vect/2,dos, '-o', linewidth = 2, label = str(i+1)+"D")
+    plt.plot(e_vect/2,dos, symbols[i],  linewidth = 5, markersize = 10, label = str(i+1)+"D")
     
 plt.legend(loc = 'best', fontsize = 30)
 plt.xlabel(r"$\omega$/D", fontsize = 30)
-plt.ylabel(r"$\rho_0(\omega)$/a.u.", fontsize = 30)
+plt.ylabel(r"$A_0(\omega)$/a.u.", fontsize = 30)
 plt.xticks(fontsize = 30)
 plt.yticks(fontsize = 30)
 plt.subplots_adjust(left = 0.13, right = 0.975, top = 0.97, bottom = 0.1, wspace = 0.1, hspace = 0.1)
@@ -26,7 +33,7 @@ fig.set_size_inches(13,10)
 #plt.savefig("DOS_fig_nk1_%s_nk2_%s_nk3_%s.pdf"%(nk1,nk2,nk3))
 plt.savefig("DOS_fig.pdf")
 
-#raw_input()
+raw_input()
 
     
             
